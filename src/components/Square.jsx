fdafbd35bc50e0fb96b3697f5bded4e6696499bc
John Michael Brown <jmichaelbrown132737@gmail.com>
2019-02-28 11:10:22 -0800

    implement history

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 
 const buttonStyle = {
-    background: '#fff',
-    border: '1px solid #999',
-    float: 'left',
-    fontSize: '24px',
-    fontWeight: 'bold',
-    lineHeight: '34px',
-    height: '34px',
-    marginRight: '-1px',
-    marginTop: '-1px',
-    padding: '0',
-    textAlign: 'center',
-    width: '34px',
-}
+  background: '#fff',
+  border: '1px solid #999',
+  float: 'left',
+  fontSize: '24px',
+  fontWeight: 'bold',
+  lineHeight: '34px',
+  height: '34px',
+  marginRight: '-1px',
+  marginTop: '-1px',
+  padding: '0',
+  textAlign: 'center',
+  width: '34px',
+};
 
 function Square(props) {
   return (
