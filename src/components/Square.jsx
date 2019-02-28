cff8d128166761bc93c7d17b4bc439155ebdb729
John Michael Brown <jmichaelbrown132737@gmail.com>
2019-02-28 09:46:23 -0800

    finish basic game setup

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square">
-        {/* TODO */}
-      </button>
-    );
-  }
+const buttonStyle = {
+    background: '#fff',
+    border: '1px solid #999',
+    float: 'left',
+    fontSize: '24px',
+    fontWeight: 'bold',
+    lineHeight: '34px',
+    height: '34px',
+    marginRight: '-1px',
+    marginTop: '-1px',
+    padding: '0',
+    textAlign: 'center',
+    width: '34px',
 }
+
+function Square(props) {
+  return (
+    <button style={buttonStyle} className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
+
+
 
 export default Square;
